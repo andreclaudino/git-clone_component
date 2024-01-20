c025be7b8ce6d7c82d6573d25cab833dd692ff77
André Claudino <claudino@d2x.com.br>
2024-01-20 11:44:10 -0300

    Setup the basics of python argo/kubeflow component

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='git-clone_component',
+    version='1.0.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='A component for cloning git repositories',
+    install_requires=[
+        "kfp~=1.8.12"
+    ]
+)
